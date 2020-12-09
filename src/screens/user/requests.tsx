8c1258614f26d0f1be75674baee8083e1b043c31
ziyafenn <ziya.fenn@gmail.com>
2020-12-10 00:14:43 +0100

    install tab navigator

--- src/screens/user/requests.tsx
+++ src/screens/user/requests.tsx
@@ -3,12 +3,41 @@ import {Text, View, Button} from 'react-native';
 import {AppContext, AuthContext} from '../../utils/context';
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 
 const db = firestore();
+const Tab = createMaterialTopTabNavigator();
 
-export default function Requests({navigation, route}) {
-  const clubRequests = route?.params.clubRequests;
-  const leagueRequests = route?.params.leagueRequests;
-  console.log(clubRequests, leagueRequests);
-  return <View></View>;
+export default function Requests({navigation}) {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Club" component={ClubRequests} />
+      <Tab.Screen name="League" component={LeagueRequests} />
+      <Tab.Screen name="Sent" component={MyRequests} />
+    </Tab.Navigator>
+  );
+}
+
+function ClubRequests() {
+  return (
+    <View>
+      <Text>Club Requests</Text>
+    </View>
+  );
+}
+
+function LeagueRequests() {
+  return (
+    <View>
+      <Text>League Requests</Text>
+    </View>
+  );
+}
+
+function MyRequests() {
+  return (
+    <View>
+      <Text>My Requests</Text>
+    </View>
+  );
 }
