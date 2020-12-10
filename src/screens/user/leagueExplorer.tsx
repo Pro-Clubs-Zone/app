068e95dbb052e42cd764ed3b383efc50024b9bfb
ziyafenn <ziya.fenn@gmail.com>
2020-12-11 00:28:41 +0100

    league expolorer

--- src/screens/user/leagueExplorer.tsx
+++ src/screens/user/leagueExplorer.tsx
@@ -2,19 +2,21 @@ import React, {useState, useEffect} from 'react';
 import {Text, View, ActivityIndicator, FlatList, Button} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import firestore from '@react-native-firebase/firestore';
+import {LeagueInt} from '../../utils/globalTypes';
 
 const Stack = createStackNavigator();
 const db = firestore();
+type Leagues = LeagueInt[] & {key: string}[];
 
-export default function LeagueExplorer({navigation}) {
-  const [data, setData] = useState([]);
+export default function LeagueExplorer({navigation}: any) {
+  const [data, setData] = useState<Leagues>([]);
   const [loading, setLoading] = useState(true);
 
   const leagueCollection = db.collection('leagues');
 
   useEffect(() => {
     const subscriber = leagueCollection.onSnapshot((querySnapshot) => {
-      let retrievedLeagues: any = [];
+      let retrievedLeagues: Leagues = [];
       querySnapshot.forEach((doc) => {
         retrievedLeagues.push({...doc.data(), key: doc.id});
       });
