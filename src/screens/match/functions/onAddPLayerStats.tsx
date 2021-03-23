e0bf47195292b0293156b856582fce49e2af9275
ziyafenn <ziya.fenn@gmail.com>
2021-03-23 23:44:56 +0100

    on add player stats

---
+++ src/screens/match/functions/onAddPLayerStats.tsx
@@ -0,0 +1,105 @@
+import firestore from '@react-native-firebase/firestore';
+import {
+  GoalkeeperStats,
+  IMatchNavData,
+  OutfieldPlayerStats,
+} from '../../../utils/interface';
+
+const db = firestore();
+
+const addPlayerStats = async (
+  match: IMatchNavData,
+  playerStats: Partial<GoalkeeperStats & OutfieldPlayerStats>,
+  uid: string,
+  isGK: boolean,
+) => {
+  const batch = db.batch();
+
+  const totalStatsRef = db
+    .collection('leagues')
+    .doc(match.leagueId)
+    .collection('stats')
+    .doc('players');
+
+  const matchStatsRef = totalStatsRef.collection('playerMatches').doc(uid);
+
+  const matchRef = db
+    .collection('leagues')
+    .doc(match.leagueId)
+    .collection('matches')
+    .doc(match.matchId);
+
+  let totalStats = {};
+
+  if (isGK) {
+    totalStats = {
+      goalsConceded: firestore.FieldValue.increment(playerStats.goalsConceded),
+      shotsCaught: firestore.FieldValue.increment(playerStats.shotsCaught),
+      shotsParried: firestore.FieldValue.increment(playerStats.shotsParried),
+      crossesCaught: firestore.FieldValue.increment(playerStats.crossesCaught),
+      ballsStriped: firestore.FieldValue.increment(playerStats.ballsStriped),
+    };
+  } else {
+    totalStats = {
+      rating: firestore.FieldValue.increment(playerStats.rating),
+      goals: firestore.FieldValue.increment(playerStats.goals),
+      shotsOnTarget: firestore.FieldValue.increment(playerStats.shotsOnTarget),
+      shotsOffTarget: firestore.FieldValue.increment(
+        playerStats.shotsOffTarget,
+      ),
+      assists: firestore.FieldValue.increment(playerStats.assists),
+      completedShortPasses: firestore.FieldValue.increment(
+        playerStats.completedShortPasses,
+      ),
+      completedMediumPasses: firestore.FieldValue.increment(
+        playerStats.completedMediumPasses,
+      ),
+      completedLongPasses: firestore.FieldValue.increment(
+        playerStats.completedLongPasses,
+      ),
+      failedShortPasses: firestore.FieldValue.increment(
+        playerStats.failedShortPasses,
+      ),
+      failedMediumPasses: firestore.FieldValue.increment(
+        playerStats.failedMediumPasses,
+      ),
+      failedLongPasses: firestore.FieldValue.increment(
+        playerStats.failedLongPasses,
+      ),
+      keyPasses: firestore.FieldValue.increment(playerStats.keyPasses),
+      successfulCrosses: firestore.FieldValue.increment(
+        playerStats.successfulCrosses,
+      ),
+      failedCrosses: firestore.FieldValue.increment(playerStats.failedCrosses),
+      keyDribbles: firestore.FieldValue.increment(playerStats.keyDribbles),
+      fouled: firestore.FieldValue.increment(playerStats.fouled),
+      successfulDribbles: firestore.FieldValue.increment(
+        playerStats.successfulDribbles,
+      ),
+      wonTackles: firestore.FieldValue.increment(playerStats.wonTackles),
+      lostTackles: firestore.FieldValue.increment(playerStats.lostTackles),
+      fouls: firestore.FieldValue.increment(playerStats.fouls),
+      penaltiesConceded: firestore.FieldValue.increment(
+        playerStats.penaltiesConceded,
+      ),
+      interceptions: firestore.FieldValue.increment(playerStats.interceptions),
+      blocks: firestore.FieldValue.increment(playerStats.blocks),
+      outOfPosition: firestore.FieldValue.increment(playerStats.outOfPosition),
+      possessionWon: firestore.FieldValue.increment(playerStats.possessionWon),
+      possessionLost: firestore.FieldValue.increment(
+        playerStats.possessionLost,
+      ),
+      clearances: firestore.FieldValue.increment(playerStats.clearances),
+      headersWon: firestore.FieldValue.increment(playerStats.headersWon),
+      heardersLost: firestore.FieldValue.increment(playerStats.heardersLost),
+    };
+  }
+
+  batch.set(totalStatsRef, {[uid]: totalStats}, {merge: true});
+  batch.set(matchStatsRef, {[match.matchId]: playerStats}, {merge: true});
+  batch.update(matchRef, {[`players.${uid}`]: true});
+
+  await batch.commit().catch((err) => console.log('error', err));
+};
+
+export default addPlayerStats;
--- src/screens/match/submitStats.tsx
+++ src/screens/match/submitStats.tsx
@@ -1,6 +1,5 @@
 import React, {useContext, useState} from 'react';
 import {ScrollView, View, Alert, ImageURISource} from 'react-native';
-import {AppContext} from '../../context/appContext';
 import {MatchStackType} from './match';
 import ScoreBoard from '../../components/scoreboard';
 import {ScaledSheet, verticalScale} from 'react-native-size-matters';
@@ -21,6 +20,9 @@ import ImageView from 'react-native-image-viewing';
 import storage, {firebase} from '@react-native-firebase/storage';
 import Toast from '../../components/toast';
 import SwitchLabel from '../../components/switch';
+import {GoalkeeperStats, OutfieldPlayerStats} from '../../utils/interface';
+import addPlayerStats from './functions/onAddPLayerStats';
+import {AuthContext} from '../../context/authContext';
 
 type ScreenNavigationProp = StackNavigationProp<MatchStackType, 'Submit Stats'>;
 
@@ -28,9 +30,45 @@ type Props = {
   navigation: ScreenNavigationProp;
 };
 
-interface SelectMenu {
-  id: string;
-}
+let outfieldPlayerStats: OutfieldPlayerStats = {
+  rating: 0,
+  goals: 0,
+  shotsOnTarget: 0,
+  shotsOffTarget: 0,
+  assists: 0,
+  completedShortPasses: 0,
+  completedMediumPasses: 0,
+  completedLongPasses: 0,
+  failedShortPasses: 0,
+  failedMediumPasses: 0,
+  failedLongPasses: 0,
+  keyPasses: 0,
+  successfulCrosses: 0,
+  failedCrosses: 0,
+  keyDribbles: 0,
+  fouled: 0,
+  successfulDribbles: 0,
+  wonTackles: 0,
+  lostTackles: 0,
+  fouls: 0,
+  penaltiesConceded: 0,
+  interceptions: 0,
+  blocks: 0,
+  outOfPosition: 0,
+  possessionWon: 0,
+  possessionLost: 0,
+  clearances: 0,
+  headersWon: 0,
+  heardersLost: 0,
+};
+
+let goalkeeperStats: GoalkeeperStats = {
+  goalsConceded: 0,
+  shotsCaught: 0,
+  shotsParried: 0,
+  crossesCaught: 0,
+  ballsStriped: 0,
+};
 
 //const db = firestore();
 
@@ -47,24 +85,29 @@ export default function SubmitStats({navigation}: Props) {
   const [showToast, setShowToast] = useState(false);
   const [toastMessage, setToastMessage] = useState('');
 
-  const context = useContext(AppContext);
   const matchContext = useContext(MatchContext);
+  const auth = useContext(AuthContext);
 
   const matchData = matchContext.match;
-  const leagueId = matchData.leagueId;
-  const clubId = matchData.clubId;
+  const uid = auth.uid;
 
-  // const leagueRef = db
-  //   .collection('leagues')
-  //   .doc(leagueId)
-  //   .collection('matches');
-
-  // const query = leagueRef
-  //   .where('homeTeamId', 'in', matchData.teams)
-  //   .where('teams', 'array-contains', '')
-  //   .where('published', '==', true);
-
-  // const getMatches = useGetMatches(leagueId, query);
+  const showAlert = () => {
+    Alert.alert(
+      i18n._(t`Stats Submitted`),
+      i18n._(t`All good`),
+      [
+        {
+          text: i18n._(t`Close`),
+          onPress: () => {
+            setLoading(false);
+            navigation.popToTop();
+            navigation.goBack();
+          },
+        },
+      ],
+      {cancelable: false},
+    );
+  };
 
   const onShowToast = (message: string) => {
     setShowToast(true);
@@ -94,6 +137,16 @@ export default function SubmitStats({navigation}: Props) {
       (image, i) => i !== seletectedThumbIndex,
     );
     setImages(updatedImages);
+  };
+
+  const onSubmitStats = async () => {
+    setLoading(true);
+    await addPlayerStats(matchData, outfieldPlayerStats, uid, isGK).then(
+      async () => {
+        await analytics().logEvent('match_submit_stats');
+        showAlert();
+      },
+    );
   };
 
   return (
@@ -144,6 +197,7 @@ export default function SubmitStats({navigation}: Props) {
             )
           }
         />
+        <ListHeading col1={i18n._(t`Options`)} />
         <View style={styles.options}>
           <SwitchLabel
             title={i18n._(t`I played as a Goalkeeper`)}
@@ -156,7 +210,7 @@ export default function SubmitStats({navigation}: Props) {
             onValueChange={() => setUpdateImage(!updateImage)}
           />
         </View>
-        <BigButton title={i18n._(t`Submit Stats`)} />
+        <BigButton title={i18n._(t`Submit Stats`)} onPress={onSubmitStats} />
       </ScrollView>
     </>
   );
